f8dd7120498ad9c275a7ce326924b379e574c525
Yoshikazuuu <febrianojerry@gmail.com>
2022-11-10 16:59:34 +0700

    bruh

--- socs_scraping.py
+++ socs_scraping.py
@@ -16,7 +16,7 @@ def getFiles():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
     # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
