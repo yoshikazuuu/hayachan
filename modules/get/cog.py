630010036d51836d4ff6ffe85935807d5a19a59f
Yoshikazuuu <febrianojerry@gmail.com>
2022-11-10 14:06:55 +0700

    some updates on get

--- modules/get/cog.py
+++ modules/get/cog.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
+from zipfile import ZipFile
 import discord
 import glob
+import os
 
 class Get(commands.Cog, name="Download"):
     """For Downloading listed files"""
@@ -9,19 +11,95 @@ class Get(commands.Cog, name="Download"):
         self.bot = bot
 
     @commands.command()
-    async def get(self, ctx: commands.Context, lab, week, problem):
+    async def get(self, ctx: commands.Context, lab, week=None, problem=None):
         """
         Get the .pdf files for the desired problem
 
         Example: 
         `haya get BG75 3 A`
-        `haya get BG75 5 D`
+        `haya get BG75 5` on progress(not yet available)
+        `haya get BG75` on progress(not yet available)
+        `haya get all`
         """
-        problem = problem.upper()
-        lab = lab.upper()
-        file_path = glob.glob("./problems/*/" + lab + '-week-' + week + '-prob-' + problem + '.pdf')
+        def get_all_file_paths(directory):
         
-        await ctx.send(file=discord.File(file_path[0]))
+            # initializing empty file paths list
+            file_paths = []
+        
+            # crawling through directory and subdirectories
+            for root, directories, files in os.walk(directory):
+                for filename in files:
+                    # join the two strings in order to form the full filepath.
+                    filepath = os.path.join(root, filename)
+                    file_paths.append(filepath)
+        
+            # returning all file paths
+            return file_paths            
+
+        def bulking():
+            # path to folder which needs to be zipped
+            directory = './problems'
+        
+            # calling function to get all file paths in the directory
+            file_paths = get_all_file_paths(directory)
+            file_paths.sort()
+        
+            # # printing the list of all files to be zipped
+            # for file_name in file_paths:
+            #     print(file_name)
+        
+            # writing files to a zipfile
+            with ZipFile('./problems/problems-' + temp  +'.zip','w') as zip:
+                print('Following files will be zipped:')
+                # writing each file one by one
+                for file in file_paths:
+                    if lab != 'all':
+                        if file.find(lab) != -1 and file.find('week-' + week): 
+                            print(file)
+                            zip.write(file)
+                        elif file.find(lab) != -1:
+                            print(file)
+                            zip.write(file)
+                        else: continue
+                    else:
+                        print(file)
+                        zip.write(file)
+
+
+            print('All files zipped successfully!') 
+
+        async def download():
+            if os.path.exists('./problems/problems-all.zip'):
+                print("File exist!")
+                await ctx.send("Uploading...")
+                await ctx.send(file=discord.File('./problems/problems-' + temp  +'.zip'))
+            else:
+                bulking()  
+                await ctx.send("Uploading...")
+                await ctx.send(file=discord.File('./problems/problems-' + temp  +'.zip'))
+
+        if lab == 'all':
+            temp = lab 
+        elif lab and week:
+            temp = lab + '-' +str(week)    
+        elif lab:
+            temp = lab
+
+        if lab == 'all':
+            print("all")
+            download()
+        elif lab and week:
+            print("lab and week")
+            download()
+        elif lab:
+            print("lab")
+            download()
+        else:
+            problem = problem.upper()
+            lab = lab.upper()
+            file_path = glob.glob("./problems/*/" + lab + '-week-' + week + '-prob-' + problem + '.pdf')
+            
+            await ctx.send(file=discord.File(file_path[0]))
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Get(bot))
